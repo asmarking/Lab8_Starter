41275fc1ed24640c433a50a41e80dc1dbbd3f274
Jackcool81 <76261988+Jackcool81@users.noreply.github.com>
2022-11-19 20:58:54 -0800

    explore part 1

--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,21 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+test('adds 1 + 2 to equal 3', () => {
+    expect(functions.isPhoneNumber("760-612-9251")).toBe(true);
+});
+
+
+//
+test('adds 1 + 2 to equal 3', () => {
+    expect(functions.isStrongPassword("1abc")).toBe(false);
+});
+test('adds 1 + 2 to equal 3', () => {
+    expect(functions.isStrongPassword("760-612-9251")).toBe(true);
+});
+test('adds 1 + 2 to equal 3', () => {
+    expect(functions.isStrongPassword("760-612-9251")).toBe(true);
+});
+test('adds 1 + 2 to equal 3', () => {
+    expect(functions.isStrongPassword("760-612-9251")).toBe(true);
+});
