5207fec00d078d4687d8e5bbd3f2cfa5abc165cf
Jackcool81 <76261988+Jackcool81@users.noreply.github.com>
2022-11-19 21:17:08 -0800

    unit testing

--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -2,22 +2,46 @@
 
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
-// TODO - Part 2
-test('adds 1 + 2 to equal 3', () => {
+// Phone Numbers
+test('Valid Phone Number', () => {
     expect(functions.isPhoneNumber("760-612-9251")).toBe(true);
 });
+test('Valid Phone Number with area code', () => {
+    expect(functions.isPhoneNumber("1+760-612-9251")).toBe(true);
+});
+test('No Dashes', () => {
+    expect(functions.isPhoneNumber("7606129251")).toBe(false);
+});
+// test('Valid Phone Number with area code', () => {
+//     expect(functions.isPhoneNumber("7213213160-6132321332132131312-922312132151")).toBe(false);
+// });
 
 
-//
-test('adds 1 + 2 to equal 3', () => {
+
+//password
+test('Password starting with number', () => {
     expect(functions.isStrongPassword("1abc")).toBe(false);
 });
-test('adds 1 + 2 to equal 3', () => {
-    expect(functions.isStrongPassword("760-612-9251")).toBe(true);
+test('Password with 3 characters', () => {
+    expect(functions.isStrongPassword("abc")).toBe(false);
 });
-test('adds 1 + 2 to equal 3', () => {
-    expect(functions.isStrongPassword("760-612-9251")).toBe(true);
+test('Valid Password with 14 Letter Characters', () => {
+    expect(functions.isStrongPassword("abcdefghijklmn")).toBe(true);
 });
-test('adds 1 + 2 to equal 3', () => {
-    expect(functions.isStrongPassword("760-612-9251")).toBe(true);
+test('Valid Password with Mixed Characters', () => {
+    expect(functions.isStrongPassword("ab_d123hij_l65")).toBe(true);
+});
+
+//Email
+test('Valid Email Address gmail.com', () => {
+    expect(functions.isEmail("jackwagner654@gmail.com")).toBe(true);
+});
+test('Valid Email Address yahoo.com', () => {
+    expect(functions.isEmail("a@yahoo.com")).toBe(true);
+});
+test('Email missing extension', () => {
+    expect(functions.isEmail("jackwagner654")).toBe(false);
+});
+test('No @ character', () => {
+    expect(functions.isEmail("jackwagner654gmail.com")).toBe(false);
 });
